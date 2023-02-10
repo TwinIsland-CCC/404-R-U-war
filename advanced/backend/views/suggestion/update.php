bde8a83b8b68253acc9156d5791e1c43ab025d18
xinkeluo <xinkeeluo@163.com>
2023-02-10 15:39:45 +0800

    comment

--- advanced/backend/views/suggestion/update.php
+++ advanced/backend/views/suggestion/update.php
@@ -1,5 +1,10 @@
 <?php
-
+/**
+ *  Team: 404NotFound
+ *  Coding by Luo XinKe 2013622
+ *            2023/2/10
+ *  Russia-Ukraine backend
+ */
 use yii\helpers\Html;
 
 /** @var yii\web\View $this */
